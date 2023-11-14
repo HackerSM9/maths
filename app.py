7662f4c64d46d90dc425684383628e32e5ec9504
HackerSM9 <sm9hacking@gmail.com>
2023-11-14 15:21:21 +0530

    .

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import sys,os
 from modules import sprint as sp
 from modules import banner as bn
+from modules import maths as mt
 from colorama import Fore,Style
 
 os.system("clear")
@@ -18,3 +19,17 @@ print("""
 
 choose = "Choose your Equation: "
 sp.sprint(input(choose))
+
+if choose == '1':
+    num1 = input('Enter the first number: ')
+    num2 = input('Enter the second number: ')
+    print(f'{num1} + {num2} = ',mt.addition(int(num1), int(num2)))
+          elif choose == '2':
+          num1 = input('Enter the first number: ')
+          num2 = input('Enter the second number: ')
+          print(f'{num1} - {num2} = ',mt.subtraction(int(num1), int(num)))
+          elif choose == '3':
+          num1 = input('Enter the first number: ')
+          num2 = input('Enter the second number: ')
+          print(f'{num1} * {num2} = ',mt.multiplication(int(num1), int(num)))
+          elif choose == '4':
